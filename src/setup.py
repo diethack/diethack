a13c1ddbf82073a49d8ee18b6081f6118e26a94a
Oleg Plakhotniuk <olegus8@gmail.com>
2015-05-01 21:54:34 -0500

    pkg

--- src/setup.py
+++ src/setup.py
@@ -15,13 +15,15 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
+        'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 2 :: Only',
-        'Topic :: Scientific/Engineering :: Medical Science Apps.'
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Topic :: Software Development :: Libraries'
     ],
     keywords='diet calculator',
     packages=find_packages(exclude=['prepare.py', 'nndb'])
